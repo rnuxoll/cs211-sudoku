78bbc4e8a439982c8a1bd42b053b9a41157f9595
Ravi Nuxoll <ravinuxoll@dhcp-10-105-135-82.wireless.northwestern.private>
2023-05-24 13:37:56 -0500

    first commit

---
+++ src/controller.cxx
@@ -0,0 +1,15 @@
+#include "controller.hxx"
+
+Controller::Controller()
+        : model_(),
+          view_(model_)
+{ }
+
+void
+Controller::draw(ge211::Sprite_set& set)
+{
+    view_.draw(set);
+}
+
+
+
---
+++ src/model.cxx
@@ -0,0 +1,9 @@
+#include "model.hxx"
+
+
+// 2D-list of numbers represents board
+// to store the candidate numbers that the user inputs, you can make a
+// parallel 2D list of NumberSets, similar to PositionSets or MoveMaps
+// where you can set, the square in row i and column j to have a candidate
+// number of "X" by model_.board[i][i][X] = true;
+
