1b40c8df11b75bea1f1400c72fedc51e2239b9ac
Ravi Nuxoll <ravinuxoll@dhcp-10-105-135-82.wireless.northwestern.private>
2023-05-25 10:28:50 -0500

    finalized what should be a working first version of board.hxx

--- src/board.hxx
+++ src/board.hxx
@@ -3,11 +3,9 @@
 #include <ge211.hxx>
 
 #include <iostream>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
 #include <array>
 #include "cell.hxx"
+#include "position_set.hxx"
 
 // citation: started with Othello board code and then modified it
 
@@ -22,132 +20,42 @@ public:
     //
     // HELPFUL TYPE ALIASES
     //
-    /// Board dimensions will use `int` coordinates.
-    using Dimensions = ge211::Dims<int>;
 
-    /// Board positions will use `int` coordinates.
+
+    // Board positions will use `int` coordinates.
     using Position = ge211::Posn<int>;
-
-
-    // TODO can probably delete this
-    /// Board rectangles will use `int` coordinates.
-    using Rectangle = ge211::Rect<int>;
-
-    // Defined and documented below.
-    class reference;
 
 private:
 
     // the sudoku board here is an array of Cell objects
     std::array<std::array<Cell, BOARD_SIZE>, BOARD_SIZE> board;
 
-    Dimensions dims_;
-
-
 public:
 
     // constructor
     Board();
 
+    // find which squares have redundant values and return them as a
+    // Position_set
+    Position_set find_contradictions() const;
+
+
+    // helper function for find_contradictions()
+    // finds all the cells that contradict the current value and returns
+    // a Position_set with the indexes of the squares that do
+    // returns an empty position_set if there are no contradicting squares
+    // Position: cell_index the index of the cell whose contradictions
+    // the function is supposed to find
+    Position_set find_contradicting_squares(Position cell_index) const;
+
     // accessor methods
     int get_cell_value(int row, int col) const;
     std::map<int, bool> get_candidates(int row, int col) const;
 
-
-
     // mutator methods
     void set_cell_value(int row, int col, int value);
+    void set_candidate(int row, int col, bool value);
 
-    //
-
-private:
-    //
-    // PRIVATE HELPER FUNCTION MEMBERS
-    //
-
-    Player get_(Position where) const;
-    void set_(Position where, Player who);
-    void bounds_check_(Position where) const;
-
-#ifdef CS211_TESTING
-    // When this class is compiled for testing, members of a struct named
-    // Test_access will be allowed to access private members of this class.
-    friend struct Test_access;
-#endif
 };
 
-
-//
-// FREE FUNCTIONS FOR WORKING WITH THE CLASS
-//
-
-/// Inequality for boards.
-bool
-operator!=(Board const&, Board const&);
-
-/// Board printing, suitable for debugging.
-std::ostream&
-operator<<(std::ostream&, Board const&);
-
-
-//
-// HELPER CLASSES
-//
-
-/// Class returned by `operator[](Position)` that simulates
-/// an assignable reference to a `Posn<int>`. This is what allows
-/// you to write
-///
-///     board[pos] = player;
-///
-/// to place `player` at `pos`.
-///
-/// The definition of the class follows this definition of the
-/// `Board` class.
-class Board::reference
-{
-    Board& board_;
-    Position pos_;
-
-public:
-    /// Assigns the value of `that` to the object of `this`.
-    reference& operator=(reference const&) noexcept;
-
-    /// Assigns to the object of the reference.
-    reference& operator=(Player) noexcept;
-
-    /// Returns the value of the reference.
-    operator Player() const noexcept;
-
-private:
-    friend class Board;
-
-    reference(Board&, Position) noexcept;
-};
-
-
-/// Class returned by `at_set(Position_set)` that allows assigning
-/// one player to all the positions in the given `Position_set`.
-class Board::multi_reference
-{
-    Board& board_;
-    Position_set pos_set_;
-
-public:
-    /// Assigns the given player to all the positions of this
-    /// multi-reference, which are all the positions in the set that
-    /// was given to `Board::at_set(Position_set)`. Thus, you can
-    /// mass-assign a player to a set of positions like so:
-    ///
-    ///     // Sets three positions to dark:
-    ///     Position_set positions{{0, 0}, {1, 1}, {2, 2}};
-    ///     board.at_set(positions) = Player::dark;
-    ///
-    multi_reference& operator=(Player) noexcept;
-
-private:
-    friend class Board;
-
-    multi_reference(Board&, Position_set) noexcept;
-};
 
--- src/model.hxx
+++ src/model.hxx
@@ -8,47 +8,19 @@
 class Model
 {
 public:
-    /// Model dimensions will use `int` coordinates
-    using Dimensions = Board::Dimensions;
 
-    /// Model positions will use `int` coordinates
-    using Position = Board::Position;
+    // helpful aliases
+    using Position
 
-    /// Model rectangles will use `int` coordinates, as board rectangles do.
-    using Rectangle = Board::Rectangle;
-
-    /// Constructs a model with `size` as both its width and height.
-    ///
-    /// ## Errors
-    ///
-    ///  - Throws `ge211::Client_logic_error` if `size` is less than 2
-    ///    or greater than 10.
-    explicit Model(int size = 10);
-
-    /// Constructs a model with the given width and height.
-    ///
-    /// ## Errors
-    ///
-    ///  - Throws `ge211::Client_logic_error` if either dimension is less
-    ///    than 2 or greater than 8.
-    Model(int width, int height);
-
-    /// Returns a rectangle containing all the positions of the board.
-    /// This can be used to iterate over the positions.
+    // Returns a rectangle containing all the positions of the board.
+    // This can be used to iterate over the positions.
     Rectangle all_positions() const;
 
-    /// Returns whether the game is finished. This is true when a correct
-    /// solution has been input
+    // Returns whether the game is finished. This is true when a correct
+    // solution has been input
     bool is_game_over() const;
 
 
-
-    /// Returns the Cell at the given position
-    ///
-    /// ## Errors
-    ///
-    ///  - Throws `ge211::Client_logic_error` if the position is out of
-    ///    bounds.
     Cell operator[](Position) const;
 
 
@@ -67,18 +39,11 @@ private:
 
     Position selected_square_;
 
-    //
-    // PRIVATE HELPER FUNCTIONS
-    //
-
-    //
-    Position_set find_contradictions_(Position start, Dimensions dir) const;
-
-    /// Sets the game status to over
-    /// to be called if a correct solution has been input
+    // set game status to over, to be called if a correct solution is
+    // implemented
     void set_game_over_();
 
-    /// modifies the inputs in the board with a user value
+    //
     void input_value(Position square, int value);
 
     /// adds a candidate number to the square
